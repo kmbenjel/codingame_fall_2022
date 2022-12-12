a4cf20df9e0bbad0453ff1e7aa913ced7fbbeb25
Khalid Benjelloun <khalidmailing@gmail.com>
2022-12-12 22:31:10 +0100

    kbenjell

--- challenge.rb
+++ challenge.rb
@@ -67,3 +67,25 @@ Your program does not provide a command in the allotted time or it provides an u
 
 
 =end
+
+STDOUT.sync = true # DO NOT REMOVE
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+width, height = gets.split(" ").collect { |x| x.to_i }
+
+# game loop
+loop do
+  my_matter, opp_matter = gets.split(" ").collect { |x| x.to_i }
+  height.times do
+    width.times do
+      # owner: 1 = me, 0 = foe, -1 = neutral
+      scrap_amount, owner, units, recycler, can_build, can_spawn, in_range_of_recycler = gets.split(" ").collect { |x| x.to_i }
+    end
+  end
+
+  # Write an action using puts
+  # To debug: STDERR.puts "Debug messages..."
+
+  puts "WAIT"
+end
