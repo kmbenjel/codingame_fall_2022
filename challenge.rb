6355ee6869b0ef034aac12da46215e9b0aab7512
Khalid Benjelloun <khalidmailing@gmail.com>
2022-12-18 10:23:03 +0100

    kbenjell

--- challenge.rb
+++ challenge.rb
@@ -1,7 +1,7 @@
 STDOUT.sync = true # DO NOT REMOVE
 
 #Find the farthest point in the field
-def farthest(w, h, x, y)
+def farthest w h, x, y)
   far_x = x > w/2 ? 0 : w
   far_y = y > h/2 ? 0 : h
   {x: far_x, y: far_y}
--- fun.rb
+++ fun.rb
@@ -2,4 +2,10 @@
 
 
 
+def farthest_point(w, h, x, y)
+  far_x = x > w/2 ? 0 : w
+  far_y = y > h/2 ? 0 : h
+  {x: far_x, y: far_y}
+end
 
+ruby3 notation instead
