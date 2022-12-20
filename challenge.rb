6e3f32b375c3af9bc522b671c4f1b4ec994cbbfb
Khalid Benjelloun <khalidmailing@gmail.com>
2022-12-20 16:36:01 +0100

    kbenjell

--- challenge.rb
+++ challenge.rb
@@ -1,5 +1,5 @@
 STDOUT.sync = true # DO NOT REMOVE
-
+$tiles = Array.new
 # Find the farthest point in the field
 def farthest(w, h, x, y)
   far_x = x > w/2 ? 0 : w
@@ -7,16 +7,27 @@ def farthest(w, h, x, y)
   { x: far_x, y: far_y }
 end
 
+# Function if_scrap that ensures that coordinates x and y are >= 1, and scrap_amount in the current tile is is >= 1.
 
+def if_scrap(x, y)
+  x >= 1 && y >= 1 && $tiles.select { |tile| tile[:x] == x && tile[:y] == y && tile[:scrap_amount] >= 1 } != []
+end
+
+# Function if_scrap_around checks if_scrap for the current tile and the four neighboring tiles
+
+def if_scrap_around(x, y)
+  if_scrap(x, y) && if_scrap(x - 1, y) && if_scrap(x + 1, y) && if_scrap(x, y - 1) && if_scrap(x, y + 1)
+end
 
 ME = 1
 OPP = 0
 NONE = -1
 
 width, height = gets.split.map &:to_i
-
+rolespawn = 0
 # game loop
 loop {
+  rolespawn += 1
   tiles = []
   my_units = []
   opp_units = []
@@ -40,15 +51,19 @@ loop {
        can_spawn: can_spawn==1,
        in_range_of_recycler: in_range_of_recycler==1,
        x: x,
-       y: y
+       y: y,
      }
 
      tiles.append(tile)
-
+     $tiles = tiles
      if tile[:owner] == ME
          my_tiles.append(tile)
          if tile[:units] > 0
-             my_units.append(tile)
+            tile[:x] == 0 ? (tile[:tx] = 0; tile[:ty] = height - 1) : false
+            tile[:y] == height - 1 ? (tile[:tx] = height - 1; tile[:ty] = width - 1) : false
+            tile[:x] == width - 1 ? (tile[:tx] = width - 1; tile[:ty] = 0) : false
+            tile[:y] == 0 ? (tile[:tx] = 0; tile[:ty] = 0) : false
+            my_units.append(tile)
          elsif tile[:recycler]
              my_recyclers.append(tile)
          end
@@ -64,35 +79,32 @@ loop {
      end
     }
   }
-  target_x = farthest(width, height, my_tiles.first[:x], my_tiles.first[:y])[:x]
-  target_y = farthest(width, height, my_tiles.first[:x], my_tiles.first[:y])[:y]
+
+
+
   actions = []
-  role = 0
   my_tiles.each { |tile|
-    role += 1
-    if tile[:can_build]
-      if !tile[:in_range_of_recycler] && role % 7 == 0
-        should_build = 1 # TODO: pick whether to build recycler here
-      else
-        should_build = false
-      end
-      if should_build
-          actions<<"BUILD #{tile[:x]} #{tile[:y]}"
-      end
-    end
-    if tile[:can_spawn]
-
-      amount = my_matter / 10 # TODO: pick amount of robots to spawn here
-
+    if tile[:can_spawn] && tile == my_tiles.first && rolespawn % 7 == 1
+      amount = my_matter / 10
       if amount > 0
           actions<<"SPAWN #{amount} #{tile[:x]} #{tile[:y]}"
       end
     end
+    if tile[:can_build]
+        if tile[:units] == 0 && if_scrap_around(tile[:x], tile[:y]) && tile != my_tiles.first
+          should_build = true
+        end
+        should_build = false
+        if should_build
+            actions<<"BUILD #{tile[:x]} #{tile[:y]}"
+            my_units.delete { |unit| unit[:x] == tile[:x] && unit[:y] == tile[:y] }
+        end
+    end
   }
-
   my_units.each { |tile|
-    target = { x: target_x, y: target_y }; # TODO: pick a destination tile
-    if target
+    # TODO: pick a destination tile
+    target = { x: tile[:tx], y: tile[:ty] }
+    if target && [tile[:x], tile[:y]] != [target[:x], target[:y]]
       amount = tile[:units] # TODO: pick amount of units to move
       actions<<"MOVE #{amount} #{tile[:x]} #{tile[:y]} #{target[:x]} #{target[:y]}"
     end
--- no_grass.rb
+++ no_grass.rb
@@ -1,4 +1,25 @@
-# Function if_scrap that ensures coordinates y and x are non zero, and ensures that scrapamount in the current tile and neighbor tiles is >= 1
-def if_scrap(x, y, tiles)
+#write a function that gives every tile a
+  # Function if_scrap that ensures that coordinates x and y are >= 1, and scrap_amount in the current tile is is >= 1
+  def if_scrap(x, y)
+    x >= 1 && y >= 1 && $tiles.select { |tile| tile[:x] == x && tile[:y] = y }.first[:scrap_amount] >= 1
+  end
 
-end
+  # Function if_scrap_around checks if_scrap for the current tile and the four neighboring tiles
+  def if_scrap_around(x, y)
+    if_scrap(x, y) || if_scrap(x - 1, y) || if_scrap(x + 1, y) || if_scrap(x, y - 1) || if_scrap(x, y + 1)
+  end
+
+  def opp_neighbor(tile)
+    neighbors = [
+      { x: tile[:x], y: tile[:y] - 1 },
+      { x: tile[:x], y: tile[:y] + 1 },
+      { x: tile[:x] - 1, y: tile[:y] },
+      { x: tile[:x] + 1, y: tile[:y] }
+    ]
+    neighbors.each do |neighbor|
+      return true if $tiles.any? { |t| t[:x] == neighbor[:x] && t[:y] == neighbor[:y] && t[:owner] == OPP }
+    end
+    return false
+  end
+
+  
