08da919d0cfee894c4e8bfb8fee7fb8a705c94c3
Khalid Benjelloun <khalidmailing@gmail.com>
2022-12-29 10:28:13 +0100

    kbenjell

--- challenge.rb
+++ challenge.rb
@@ -6,17 +6,7 @@ STDOUT.sync = true # DO NOT REMOVE
 #ALLAH
 
 
-def opp_not_far(opp_tiles, my_tiles)
-  close_tiles = []
-  opp_tiles.each do |opp_tile|
-    my_tiles.each do |my_tile|
-      if (my_tile[:x] - opp_tile[:x]).abs + (my_tile[:y] - opp_tile[:y]).abs <= 2
-        close_tiles << my_tile
-      end
-    end
-  end
-  return close_tiles
-end
+
 
 # Check if I have scrap in the current tile and the neighboring tiles
 def scrap_around(tiles, tile)
